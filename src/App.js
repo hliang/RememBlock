69fd6643f87570769efa5caba2c10b4e1d532e37
hliang <hliang@github>
2019-04-27 23:46:29 -0500

    feat: new sets of images

--- src/App.js
+++ src/App.js
@@ -11,13 +11,20 @@ function shuffle(a) {
   }
   return a;
 }
+const imgPools = [
+  {"name": "Letters", "imgArray": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]},
+  {"name": "Miss Korean", "imgArray": ["mk_0", "mk_1", "mk_10", "mk_11", "mk_12", "mk_13", "mk_14", "mk_15", "mk_16", "mk_17", "mk_2", "mk_3", "mk_4", "mk_5", "mk_6", "mk_7", "mk_8", "mk_9"]},
+  {"name": "Dog Head", "imgArray": ["dh_0", "dh_1", "dh_10", "dh_11", "dh_12", "dh_13", "dh_14", "dh_15", "dh_2", "dh_3", "dh_4", "dh_5", "dh_6", "dh_7", "dh_8", "dh_9"]},
+  {"name": "Fish", "imgArray": ["fish_0", "fish_1", "fish_10", "fish_11", "fish_12", "fish_13", "fish_14", "fish_15", "fish_16", "fish_17", "fish_18", "fish_19", "fish_2", "fish_3", "fish_4", "fish_5", "fish_6", "fish_7", "fish_8", "fish_9"]},
+  {"name": "Lips", "imgArray": ["lips_1", "lips_10", "lips_11", "lips_12", "lips_13", "lips_14", "lips_15", "lips_16", "lips_17", "lips_18", "lips_19", "lips_2", "lips_20", "lips_3", "lips_4", "lips_5", "lips_6", "lips_7", "lips_8", "lips_9"]}
+]
 
 class App extends Component {
   state = {
     currScore: 0,
     topScore: 0,
     // Initialize blocksArr
-    blocksArr: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"],
+    blocksArr: shuffle(imgPools[Math.floor(Math.random()*imgPools.length)].imgArray).slice(0, 12),
     // blocksArr: ["a", "b", "c", "d", "e"],
     alreadyClickedArr: [],
     message: "Click a block to start the game",
@@ -61,6 +68,7 @@ class App extends Component {
       alreadyClickedArr: [],
       message: "You already clicked that. Game Over!",
       shake: true,
+      showHint: false,
     })
   }
 
